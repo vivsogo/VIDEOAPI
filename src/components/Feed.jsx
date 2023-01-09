bb0adfa785749afb2390f73f796d1e76ac6902dc
Vivian Gomez <viviangomez0@gmail.com>
2023-01-09 17:05:20 -0500

    added box and styling

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,6 +1,6 @@
 //import { useState, useEffect } from "react"
 import {Box, Stack, Typography } from '@mui/material';
-import { Sidebar } from "./"; 
+import { Sidebar ,Videos } from "./"; 
 const Feed = () => {
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
@@ -9,6 +9,13 @@ const Feed = () => {
         <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
           Copyright © 2023 Vivian Codes
         </Typography>
+      </Box>
+
+      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+        <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
+          New <span style={{ color: "#FC1503" }}>videos</span>
+        </Typography>
+        <Videos />
       </Box>
 
     </Stack>
