9c1c8fc4d66df7a1028331d90583921471c59c09
Vivian Gomez <viviangomez0@gmail.com>
2023-01-09 17:05:49 -0500

    added to sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,7 @@
 import { Stack } from '@mui/material';
 import { categories } from '../Utils/constants';
 
+const selectedCategory = "New";
 const Sidebar = () => (
    <Stack direction="row"
    sx={{
@@ -10,9 +11,18 @@ const Sidebar = () => (
    }}
  > 
    {categories.map((category) => (
-      <button>
-        <span>{category.icon}</span>
-        <span>{category.name}</span>
+      <button 
+      className="category-btn"
+      //onClick={() => setSelectedCategory(category.name)}
+      style={{
+        background: category.name === selectedCategory && "#FC1503",
+        color: "white", 
+      }}
+      key={category.name}
+      //when we map in react we have to give it a key 
+    > 
+        <span style={{ color: category.name === selectedCategory ? "white" : "red", marginRight: "15px" }}> {category.icon}</span>
+        <span style={{ opacity: category.name === selectedCategory ? "1" : "0.8" }}> {category.name}</span>
       </button>
    ))}
  
