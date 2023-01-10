d8a6139d51d49a0191f0fa96a81fe5cd4b361b5e
Vivian Gomez <viviangomez0@gmail.com>
2023-01-10 17:12:08 -0500

    passed down the selected cat prop

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,7 @@
 import { Stack } from '@mui/material';
 import { categories } from '../Utils/constants';
 
-const selectedCategory = "New";
-const Sidebar = () => (
+const Sidebar = ({ selectedCategory ,setSelectedCategory}) => (
    <Stack direction="row"
    sx={{
      overflowY: "auto",
@@ -13,7 +12,7 @@ const Sidebar = () => (
    {categories.map((category) => (
       <button 
       className="category-btn"
-      //onClick={() => setSelectedCategory(category.name)}
+      onClick={() => setSelectedCategory(category.name)}
       style={{
         background: category.name === selectedCategory && "#FC1503",
         color: "white", 
