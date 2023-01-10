2a4507e3f567dff8cb4691bc8e13081e28e1bf96
Vivian Gomez <viviangomez0@gmail.com>
2023-01-10 17:11:30 -0500

    added use state to grap api infor

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,11 +1,20 @@
-//import { useState, useEffect } from "react"
+import { useState, useEffect } from "react"
 import {Box, Stack, Typography } from '@mui/material';
+import { fetchFromAPI } from '../Utils/fetchFromAPI';
 import { Sidebar ,Videos } from "./"; 
 const Feed = () => {
+  const [selectedCategory, setSelectedCategory] = useState("New");
+  const [videos, setVideos] = useState(null);
+  useEffect(() => {
+    setVideos(null);
+
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+      .then((data) => setVideos(data.items))
+    }, [selectedCategory]);
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
       <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 } }}>
-        <Sidebar/>
+        <Sidebar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
         <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
           Copyright © 2023 Vivian Codes
         </Typography>
@@ -15,7 +24,7 @@ const Feed = () => {
         <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
           New <span style={{ color: "#FC1503" }}>videos</span>
         </Typography>
-        <Videos />
+        <Videos videos = {videos}/>
       </Box>
 
     </Stack>
