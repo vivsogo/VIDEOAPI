9244b0b33524b5ed33c63ae9da7082a58cdeb166
Vivian Gomez <viviangomez0@gmail.com>
2023-01-11 15:07:08 -0500

    added ti the feed page

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -22,9 +22,9 @@ const Feed = () => {
 
       <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
         <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
-          New <span style={{ color: "#FC1503" }}>videos</span>
+          {selectedCategory} <span style={{ color: "#FC1503" }}>videos</span>
         </Typography>
-        <Videos videos = {videos}/>
+        <Videos videos ={videos}/>
       </Box>
 
     </Stack>
