121a9672b2115e1cb0f00e4353fb72d603e3aa97
Vivian Gomez <viviangomez0@gmail.com>
2023-01-12 16:11:39 -0500

    added consol.log to check for fail

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -3,7 +3,7 @@ import { Stack, Box } from "@mui/material";
 import { ChannelCard, VideoCard } from "./";
 
 const Videos = ({videos}) => {
-  console.log(videos);
+  
     
   return (
    <Stack  direction= "row" lexWrap="wrap" justifyContent="start"  gap={2}>
