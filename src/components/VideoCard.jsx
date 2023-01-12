10993e6b0840f9fee61fe2dedffe487a3fed5a77
Vivian Gomez <viviangomez0@gmail.com>
2023-01-12 17:33:11 -0500

    added video card

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Link } from "react-router-dom"; 
+import { Typography, Card, CardContent, CardMedia } from "@mui/material";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelUrl, demoChannelTitle } from "../Utils/constants";
 
-const VideoCard = () => {
+const VideoCard = ({video: { id:{ videoId}, snippet}}) => {
+  
   return (
-    <div>VideoCard</div>
+    <Card>
+      <Link>
+        <CardMedia image={snippet?. thumbnails?.high?.url} />
+      </Link>
+    </Card>
   )
 }
 
