455b346e209bca7e99de1847a4421d7324d52542
Vivian Gomez <viviangomez0@gmail.com>
2023-01-13 21:05:23 -0500

    showed media on page

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import { Link } from "react-router-dom"; 
 import { Typography, Card, CardContent, CardMedia } from "@mui/material";
-import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import {CheckCircleIcon} from "@mui/icons-material/CheckCircle";
 import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelUrl, demoChannelTitle } from "../Utils/constants";
 
 const VideoCard = ({video: { id:{ videoId}, snippet}}) => {
   
   return (
     <Card>
-      <Link to={videoId?}>
-        <CardMedia image={snippet?. thumbnails?.high?.url} />
+      <Link to={videoId ? `/video/${videoId}` : demoVideoUrl }>
+        <CardMedia image={snippet?. thumbnails?.high?.url} 
+        alt={snippet?.title} 
+        sx={{ width: { xs: '100%', sm: '358px'}, height: 180 }} />
+        
       </Link>
     </Card>
   )
