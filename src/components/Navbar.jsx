f4cf3f2887c5b50be7a16b7545b314a15d4bae7a
Vivian Gomez <viviangomez0@gmail.com>
2023-01-16 15:13:24 -0500

    added logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,8 +5,8 @@ import SearchBar from './SearchBar';
 const Navbar = () => (
   <Stack direction="row" alignItems="center" p={2} sx={{ position:  "sticky", background: '#000', top: 0, justifyContent: "space-between" }}>  
     <Typography fontWeight="bold" color="#FFF"> VivTUBE </Typography>
-    <Link to="/" style={{ display: "flex", alignItems: "center" }}>
-      <img src={logo} alt="logo" height={45} />
+    <Link to="/">
+   <img style={{width: 60, height: 60, borderRadius: 60/ 2}} src={logo} alt="logo" />
     </Link>
     <SearchBar />
   </Stack>
