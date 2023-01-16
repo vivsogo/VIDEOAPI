8e86ce63d33c7f9957af34c89f0cdf7b6903d86d
Vivian Gomez <viviangomez0@gmail.com>
2023-01-16 14:41:44 -0500

    deleted channel card

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Stack, Box } from "@mui/material";
-import { VideoCard , ChannelCard} from "./"
+import { VideoCard } from "./"
 
 const Videos = ({videos, direction}) => {
   if(!videos?.length) return "YES"
