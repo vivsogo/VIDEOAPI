51915ddf46ce367ad7ffd444fa01b8c850c25d38
Vivian Gomez <viviangomez0@gmail.com>
2023-01-17 14:18:39 -0500

    started working on the search button

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { Typography, Box } from "@mui/material";
+import { useParams } from "react-router-dom";
+import { fetchFromAPI } from "../Utils/fetchFromAPI";
+import { Videos } from "./";
 
 const SearchFeed = () => {
-  return (
-    <div>SearchFeed</div>
-  )
-}
+  const [videos, setVideos] = useState(null);
+  
 
-export default SearchFeed
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=`)
+      .then((data) => setVideos(data.items))
+  }, []);
+
+  return (
+    <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+        <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
+          Search results for: <span style={{ color: "#FC1503" }}>videos</span>
+        </Typography>
+        <Videos videos ={videos}/>
+      </Box>
+
+  );
+};
+
+export default SearchFeed;
+
+
