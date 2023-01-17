1578cfaaf5d359e8e5329f2f0054cb3b044895c9
Vivian Gomez <viviangomez0@gmail.com>
2023-01-17 14:31:59 -0500

    added search tearm

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -6,17 +6,17 @@ import { Videos } from "./";
 
 const SearchFeed = () => {
   const [videos, setVideos] = useState(null);
-  
+  const { searchTerm } = useParams();
 
   useEffect(() => {
-    fetchFromAPI(`search?part=snippet&q=`)
+    fetchFromAPI(`search?part=snippet&q=${searchTerm}`)
       .then((data) => setVideos(data.items))
-  }, []);
+  }, [searchTerm]);
 
   return (
     <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
         <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
-          Search results for: <span style={{ color: "#FC1503" }}>videos</span>
+          Search results for: <span style={{ color: "#FC1503" }}>{searchTerm}</span> vids on VIVTUBE
         </Typography>
         <Videos videos ={videos}/>
       </Box>
